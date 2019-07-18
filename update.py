0f8415a008c04fdb4e8930c503902c642b6a31af
Office-PC <WenanLee@outlook.com>
2019-07-18 17:34:36 +0800

    重命名

---
+++ update.py
@@ -0,0 +1,29 @@
+import zipfiles
+import urllib.request
+import urllib
+import json
+
+def listzipfilesinfo(path):
+    z=zipfiles.ZipFile(path,'r')
+    for file in z.namelist():
+        z.extract(file,"./")
+        
+def update():
+    page = urllib.request.urlopen("http://www.ugmax.cn/psychology/version.html")
+    serverVersion=page.read().decode()
+    localVersion=0
+    with open('version.txt',"r") as f:    #设置文件对象
+        localVersion = f.read()
+        print("网络版本:",serverVersion," <------> 本地版本:",localVersion)
+        if serverVersion==localVersion:
+            return "暂时没有新版本"
+        else:
+            urllib.request.urlretrieve("http://www.ugmax.cn/psychology/psychology.zip", "tmp.zip")
+            listzipfilesinfo('tmp.zip')
+        f.close()
+    with open('version',"w+") as f:
+        print(serverVersion)
+        f.write(serverVersion)
+        f.close()
+    return "更新完成"
+update()
